684a60169bfa8810d01b90ef20c4cc07ed4986c9
daku556 <77696417+daku556@users.noreply.github.com>
2025-05-25 11:34:36 +0900

    05.25

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,13 +5,13 @@ import '../screens/upload_screen.dart';
 import '../screens/result_screen.dart';
 import '../screens/guide_screen.dart';
 import '../screens/analyze_screen.dart';
-// import '../screens/checklist_screen.dart';
+import '../screens/checklist_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => HomeScreen(),
   '/upload': (context) => UploadScreen(),
   '/analyze': (context) => AnalyzeScreen(),
   '/result': (context) => ResultScreen(),
-  // '/checklist': (context) =>ChecklistScreen(),
+  '/checklist': (context) => ChecklistScreen(),
   '/guide': (context) => GuideScreen(),
 };
